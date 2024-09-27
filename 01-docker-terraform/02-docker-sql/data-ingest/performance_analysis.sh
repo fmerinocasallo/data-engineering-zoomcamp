58868795398e6ff1341727b3581d36f39bc61a7d
Francisco Merino-Casallo <francisco.merinocasallo+github@gmail.com>
2024-09-27 21:30:14 +0200

    Add data ingest performance analysis.
    We are focused on tweaking the data ingest method and chunk sizes for
    both downloading the NYC taxi data from the given URL and ingesting this
    data into a new db from our PostgreSQL server.

---
+++ 01-docker-terraform/02-docker-sql/data-ingest/performance_analysis.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+for chunk_size_dw (10 100 1000 10000 100000 1000000 10000000 100000000)
+do
+    for chunk_size_sql (10 100 1000 10000 100000 1000000 10000000 100000000)
+    do
+        for method ("multi" "psql_insert_copy" "None")
+        do
+            echo "chunk_size_dw=${chunk_size_dw} - chunk_size_sql=${chunk_size_sql} - method=${method}"
+            tex_mem python data-nyc-taxi/data-manager.py \
+                --url=https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet \
+                --fname=yellow_tripdata_2021-01.parquet \
+                --username=fmerinocasallo_writer \
+                --password=pg-server/passwds/pg-fmerinocasallo_writer-passwd.txt \
+                --host=172.19.0.70 \
+                --port=10864 \
+                --db=de_zoomcamp \
+                --schema=nyc_taxi \
+                --table=yellow_taxi_trips \
+                --chunk-size-dw=${chunk_size_dw} \
+                --chunk-size-sql=${chunk_size_sql} \
+                --method-sql=${method} \
+            | grep "Took"
+        done
+    done
+done
