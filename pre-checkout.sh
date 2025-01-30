1f628daf9c181f50a8368c2b60d1a2c032e2928f
Francisco Merino-Casallo <francisco.merinocasallo+github@gmail.com>
2025-01-30 19:43:08 +0100

    Update pre-checkout.sh script.
    The pre-checkout.sh script must behave differently depending on when it
    is being executed, and it must be run before and after changing to
    another branch.

--- pre-checkout.sh
+++ pre-checkout.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Git cannot manage files owned by other users (e.g., 5050:root and 70:70).
 # However, TLS private key certificates and pgAdmin password file must be
 # owned by those users for security reasons. Therefore, we tell git to stop
 # monitoring changes to those files using the `--skip-worktree`. However,
 # we need to revert this action before checking out to other git branch.
+
+set -euo pipefail
+
+unset mode
 
 ignored=(
   "01-docker-terraform/01-docker-sql/pg-server/certs/server/server.key"
@@ -24,22 +28,72 @@ ownership=(
   "5050:root"
 )
 
-for file in "${ignored[@]}"; do
-  echo "git update-index --no-skip-worktree -- \"${file}\""
-  git update-index --no-skip-worktree -- "${file}"
-  echo "sudo chown `id -u`:`id -g` \"${file}\""
-  sudo chown `id -u`:`id -g` "${file}"
+usage(){
+>&2 cat << EOF
+Usage: $0
+   [ --mode pre|post ]
+EOF
+exit 1
+}
+
+args=$(getopt -a -o h -l \
+help,mode: \
+-- "$@")
+
+if [[ $? -gt 0 ]]; then
+    usage
+fi
+
+eval set -- ${args}
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    '-h' | '--help')
+        usage
+        shift
+        continue
+    ;;
+    '--mode')
+        mode=$2
+        shift 2
+        continue
+    ;;
+    '--')
+        shift
+        break
+    ;;
+    *)
+        >&2 echo "Unsupported option: $1"
+        usage
+    ;;
+  esac
 done
 
-echo -e "Stashing changes..."
-git stash save "Ownership changes for security reasons."
-echo "done"
-git checkout -m $@
-git stash pop
+ready=1
+if [[ ! -v mode ]]; then
+    echo "Missing mode"
+    ready=0
+fi
 
-for idx in "${!ignored[@]}"; do
-  echo "git update-index --skip-worktree -- \"${ignored[${idx}]}\""
-  git update-index --skip-worktree -- "${ignored[${idx}]}"
-  echo "sudo chown ${ownership[${idx}]} \"${ignored[${idx}]}\""
-  sudo chown {ownership[${idx}]} "${ignored[${idx}]}"
-done
+case $mode in
+    'pre')
+        echo -n "Updating git indices and files ownership..."
+        for file in "${ignored[@]}"; do
+            git update-index --no-skip-worktree -- "${file}"
+            sudo chown `id -u`:`id -g` "${file}"
+        done
+        echo "done"
+        echo "You are ready to change to another branch!"
+    ;;
+    'post')
+        echo -n "Updating git indices and files ownership..."
+        for idx in "${!ignored[@]}"; do
+            git update-index --skip-worktree -- "${ignored[${idx}]}"
+            sudo chown "${ownership[${idx}]}" "${ignored[${idx}]}"
+        done
+        echo "done"
+        echo "You are ready to run Docker containers from module-1!"
+    ;;
+    *)
+        echo "Unsupported mode: $mode. Valid modes are pre and post."
+        usage
+esac
