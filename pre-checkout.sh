48c1dc4cc6b55b028f6e5a83157880d1ab1d2335
Francisco Merino-Casallo <francisco.merinocasallo+github@gmail.com>
2025-01-30 13:58:44 +0100

    Update pre-checkout.sh script.

--- pre-checkout.sh
+++ pre-checkout.sh
@@ -6,18 +6,40 @@
 # monitoring changes to those files using the `--skip-worktree`. However,
 # we need to revert this action before checking out to other git branch.
 
-ignored=( $(git ls-files -v | grep "^S " | cut -c3-) )
+ignored=(
+  "01-docker-terraform/01-docker-sql/pg-server/certs/server/server.key"
+  "01-docker-terraform/01-docker-sql/pgadmin/certs/client/reader/fmerinocasallo_reader.key"
+  "01-docker-terraform/01-docker-sql/pgadmin/certs/client/superuser/postgres.key"
+  "01-docker-terraform/01-docker-sql/pgadmin/certs/client/writer/fmerinocasallo_writer.key"
+  "01-docker-terraform/01-docker-sql/pgadmin/certs/server/server.key"
+  "01-docker-terraform/01-docker-sql/pgadmin/passwds/.pgpass"
+)
 
-for x in "${ignored[@]}"; do
-  git update-index --no-skip-worktree -- "$x"
+ownership=(
+  "70:70"
+  "5050:root"
+  "5050:root"
+  "5050:root"
+  "5050:root"
+  "5050:root"
+)
+
+for file in "${ignored[@]}"; do
+  git update-index --no-skip-worktree -- "${file}"
+  echo "git update-index --no-skip-worktree -- \"${file}\""
+  sudo chown `id -u`:`id -g` "${file}"
+  echo "sudo chown `id -u`:`id -g` \"${file}\""
 done
 
+echo -e "Stashing changes..."
 git stash save "Ownership changes for security reasons."
-
+echo "done"
 git checkout -m $@
-
 git stash pop
 
-for x in "${ignored[@]}"; do
-  git update-index --skip-worktree -- "$x"
+for idx in "${!ignored[@]}"; do
+  git update-index --skip-worktree -- "${ignored[${idx}]}}"
+  echo "git update-index --skip-worktree -- \"${ignored[${idx}]}\""
+  sudo chown {ownership[${idx}]} "${ignored[${idx}]}"
+  echo "sudo chown ${ownership[${idx}]} \"${ignored[${idx}]}\""
 done
